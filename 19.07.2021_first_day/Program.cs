38711a5349b3613f05c34c7eb044e4174116ef12
VVaryag <78527118+VVaryag-1@users.noreply.github.com>
2021-07-19 13:18:50 +0300

    Добавьте файлы проекта.

---
+++ 19.07.2021_first_day/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// First Day - 1
+/*
+* My first program))
+*/
+
+namespace _19._07._2021_first_day
+{
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
